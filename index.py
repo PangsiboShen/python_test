6ca5c4d4c118b963ad55c628e26d5886ed7422a0
PangsiboShen <pangsiboshen0522@gmail.com>
2020-11-07 16:38:44 -0500

    exported a new csv

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 filepath = './data/Knee.csv'
-knee = pd.read_csv(filepath)
-knee.info()
+col_names = ['Below Average', 'Average', 'Above Average']
+knee = pd.read_csv(filepath, header = None, names = col_names)
+print(knee.iloc[0: , :])
+
+out_csv = 'new_knee'
+knee.to_csv(out_csv)

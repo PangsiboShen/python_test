581b69771dcf7ada7b29a6787e8c44aefef1a107
PangsiboShen <pangsiboshen0522@gmail.com>
2020-11-07 16:47:46 -0500

    added a plot

--- index.py
+++ index.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 filepath = './data/Knee.csv'
 col_names = ['Below Average', 'Average', 'Above Average']
-knee = pd.read_csv(filepath, header = None, names = col_names)
+knee = pd.read_csv(filepath, header = 0, names = col_names)
 print(knee.iloc[0: , :])
 
 out_csv = 'new_knee'
 knee.to_csv(out_csv)
+
+col_1 = knee['Below Average'].values
+plt.plot(col_1)
+plt.show()
